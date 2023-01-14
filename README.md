39df3eb59342b02d9d169c6540e90082ec952abb
Dominik Zabinski <dominikzabinski@gmail.com>
2023-01-14 16:02:40 +0100

    First step - creating data
    adding README and files for creating data

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # example_rshiny
+
+Repository for an examplatory RShiny application structured as follows
+
+data - creating database
+sample - RShiny application
+apppackage - package
+
+## data
+
+## package
+
+## application
+
+Justyfying choices for packages:
+
+- shiny, for, well, shiny
+- DBI - for connecting to database easy, simple, 
+- shinyWidgets - pickerInput mostly (opcja inline sprawia ze tworzymy human-readable zdania),
+- htmltools - for tagAppendAttributes
+- data.table for data wragnling
+- ggplot2
---
+++ data/cr.R
@@ -0,0 +1,38 @@
+# creating database
+# libraries ----
+library(DBI)
+library(data.table)
+
+# reading data ----
+musicPeople <- data.table(read.csv2(file = "data.csv", stringsAsFactors = F))
+
+# creating dictionairies ----
+genreDict <- data.table(genre = unique(musicPeople$genre))
+genreDict$genre_id <- 1:nrow(genreDict)
+
+typeDict <- data.table(type = unique(musicPeople$type))
+typeDict$type_id <- 1:nrow(typeDict)
+
+# leaving only necessary columns in the main table ----
+musicPeople$id <- 1:nrow(musicPeople)
+musicPeople <- musicPeople[typeDict, on = .(type)][genreDict, on = .(genre)][,.(id, name, genre_id, type_id)]
+
+# creating random relationships ----
+rels <- data.table(expand.grid(idA = musicPeople$id, idB = musicPeople$id))
+rels[, id_A := pmin(idA, idB)]
+rels[, id_B := pmax(idA, idB)]
+rels <- unique(rels[id_A != id_B][,.(id_A, id_B)])
+rels <- rels[sample(1:nrow(rels), size = ceiling(.15 * nrow(rels)))]
+rels$id <- 1:nrow(rels)
+
+# saving data in .sqlite database ----
+con <- dbConnect(drv = RSQLite::SQLite(), "mydatabase.sqlite")
+dbWriteTable(conn = con, name = "music_people", value = musicPeople)
+dbWriteTable(conn = con, name = "type_dict", value = typeDict)
+dbWriteTable(conn = con, name = "genre_dict", value = genreDict)
+dbWriteTable(conn = con, name = "rels", value = rels)
+
+dbListTables(con)
+
+dbDisconnect(conn = con)
+# copy it to app directory
